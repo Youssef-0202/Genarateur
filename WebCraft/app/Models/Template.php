764c2b2d5f905ac2490a877e13252a5e3ebaf0ff
Chaimaa Elaffas <chaimaaelaffas3@gmail.com>
2024-11-25 11:02:56 +0100

    commit

--- WebCraft/app/Models/Template.php
+++ WebCraft/app/Models/Template.php
@@ -10,7 +10,7 @@ class Template extends Model
     use HasFactory;
 
     protected $fillable = [
-        'nom', 'description','image'
+        'nom', 'description','image','path'
     ];
 
     public function website()
