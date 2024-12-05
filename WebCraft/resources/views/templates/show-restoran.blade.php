52d264246ce6466b151c929ab9b0fd8be88c9a7d
Chaimaa Elaffas <chaimaaelaffas3@gmail.com>
2024-12-05 22:34:41 +0100

    commit

--- WebCraft/resources/views/templates/show-restoran.blade.php
+++ WebCraft/resources/views/templates/show-restoran.blade.php
@@ -305,7 +305,7 @@
                         <div class="col-lg-6 text-center text-lg-start">
                             <h1 class="display-3 text-white animated slideInLeft">Enjoy Our<br>Delicious Meal</h1>
                             <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                            <a href="" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Book A Table</a>
+                            <a href=""  class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Book A Table</a>
                         </div>
                         <div class="col-lg-6 text-center text-lg-end overflow-hidden">
                             <img class="img-fluid" src="{{ asset('restoran/img/hero.png') }}" alt="">
@@ -762,7 +762,7 @@
 
 
         <!-- Reservation Start -->
-        <div class="container-xxl py-5 px-0 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container-xxl py-5 px-0 wow fadeInUp" data-wow-delay="0.1s" id="book-table">
             <div class="row g-0">
                 <div class="col-md-6">
                     <div class="video">
@@ -1056,6 +1056,10 @@
 
     <!-- Template Javascript -->
     <script src="{{ asset('restoran/js/main.js') }}"></script>
+
+
+
+
 </body>
 
 </html>
